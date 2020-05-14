59ab2a869612ed19c3e22584d9130b9b10f12cea
Karl Muller <karlmuller10@gmail.com>
2020-05-13 21:18:53 -0400

    Created a method for Stage 1
    forwardPath(), printPath(), and stageOne() methods created

--- RabbitMain.java
+++ RabbitMain.java
@@ -16,10 +16,40 @@ public class RabbitMain
             for(int j = 0; j < path[i].length; j++)
                 path[i][j] = 0;
     }
-    
+
+    public static void stageOne(int path[][])
+    {
+        /* Stage 1 Path on a 3x3
+          { {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1} } 
+        */
+        forwardPath(path, 3, 0);
+        setOneBlockOnPath(path, 1, 0);
+        setOneBlockOnPath(path, 1, 2);
+        forwardPath(path, 3, 2);
+    }
+
     public static void setOneBlockOnPath(int path[][], int row, int column)
     {
         path[row][column] = 1;
+    }
+
+    public static void forwardPath(int path[][], int amntOfSpaces, int row)
+    {
+        for (int i = 0; i < amntOfSpaces; i++)
+            setOneBlockOnPath(path, row, i);
+    }
+
+    public static void printPath(int path[][])
+    {
+        for (int i = 0; i < path.length; i++)
+        {
+            for (int j = 0; j < path[j].length; j++)
+                System.out.print(path[i][j] + " ");
+            
+            System.out.println();
+        }
     }
 
 }
